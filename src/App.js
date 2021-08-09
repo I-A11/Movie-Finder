c5e83c6b3a3312f6db3ecedf4ffe0186b1a971da
I-A11 <d.islam1983@gmail.com>
2021-08-09 11:48:00 +1000

    create movie components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
+import Movie from "./components/Movie";
 import Navbar from "./components/navbar/navbar";
 import Hero from "./components/sections/Hero";
 
 function App() {
+  const movies = ["1", "2", "3"];
   return (
     <div className='App'>
       <Navbar />
       <Hero />
+      {movies.map((movie) => (
+        <Movie />
+      ))}
     </div>
   );
 }
---
+++ src/components/Movie.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Movie = () => {
+  return <div className='movie'>Movies are ready hooooo</div>;
+};
+
+export default Movie;
