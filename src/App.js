c49d7c990d0512f7dbfcd2442f9b66672dbbfaa0
I-A11 <d.islam1983@gmail.com>
2021-08-09 16:13:46 +1000

    create movie components and fetch api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Movie from "./components/Movie";
 import Navbar from "./components/navbar/navbar";
 import Hero from "./components/sections/Hero";
 
+//Apis
+const FEATURED_API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
+
 function App() {
-  const movies = ["1", "2", "3"];
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(FEATURED_API);
+      const moviesR = await response.json();
+      console.log(moviesR);
+      setMovies(moviesR.results);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div className='App'>
       <Navbar />
       <Hero />
-      {movies.map((movie) => (
-        <Movie />
-      ))}
+      <div className='movie-container'>
+        {movies.length > 0 &&
+          movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+      </div>
     </div>
   );
 }
