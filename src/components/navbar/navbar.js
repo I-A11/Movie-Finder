6864087acde70b3a7918fe62e3f0682b4347cf17
I-A11 <d.islam1983@gmail.com>
2021-08-09 11:28:31 +1000

    create react app movie-finder

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <div className='nav-container'>
+          <div className='logo'>
+            <h2>MovieFinder</h2>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/components/sections/Hero.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <>
+      <section className='hero'>
+        <h2>Search Movies</h2>
+      </section>
+    </>
+  );
+};
+
+export default Hero;
