0c010e1aee307f632c1d5ff398863727cf0f8b51
I-A11 <d.islam1983@gmail.com>
2021-08-09 20:16:19 +1000

    finishing search component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Movie from "./components/Movie";
 import Navbar from "./components/navbar/navbar";
-import Hero from "./components/sections/Hero";
 
 //Apis
 const FEATURED_API =
@@ -12,6 +11,7 @@ const SEARCH_API =
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -23,10 +23,39 @@ function App() {
     fetchData();
   }, []);
 
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    async function fetchData() {
+      const response = await fetch(SEARCH_API + searchTerm);
+      const moviesR = await response.json();
+      console.log(moviesR);
+      setMovies(moviesR.results);
+    }
+    fetchData();
+    setSearchTerm("");
+  };
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
   return (
     <div className='App'>
       <Navbar />
-      <Hero />
+      <section className='hero'>
+        <h2>Search Movies</h2>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            type='search'
+            placeholder='Search'
+            className='search'
+            value={searchTerm}
+            onChange={handleOnChange}
+          />
+        </form>
+      </section>
+
       <div className='movie-container'>
         {movies.length > 0 &&
           movies.map((movie) => <Movie key={movie.id} {...movie} />)}
